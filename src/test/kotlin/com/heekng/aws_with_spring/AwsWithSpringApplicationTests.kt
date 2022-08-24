ba435ecda3805a515d9b86c5b0f62ce5a0e66475
heekng <rhkd1769@gmail.com>
2022-08-24 19:51:26 +0900

    테스트 실행 클래스명 변경

--- src/test/kotlin/com/heekng/aws_with_spring/AwsWithSpringApplicationTests.kt
+++ src/test/kotlin/com/heekng/aws_with_spring/AwsWithSpringApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringwithawsApplicationTests {
+class AwsWithSpringApplicationTests {
 
 	@Test
 	fun contextLoads() {
