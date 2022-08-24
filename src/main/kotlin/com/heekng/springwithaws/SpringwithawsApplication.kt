d9eb97c0e8b0e9b96f5fae4344454e659a4e60be
heekng <rhkd1769@gmail.com>
2022-08-24 16:21:56 +0900

    initial commit

---
+++ src/main/kotlin/com/heekng/springwithaws/SpringwithawsApplication.kt
@@ -0,0 +1,11 @@
+package com.heekng.springwithaws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringwithawsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringwithawsApplication>(*args)
+}
---
+++ src/test/kotlin/com/heekng/springwithaws/SpringwithawsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.heekng.springwithaws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringwithawsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
